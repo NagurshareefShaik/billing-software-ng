7f68c69d82805aa36d80a5958bc17176a9951d7c
Nagurshareef Shaik <42448704+NagurshareefShaik@users.noreply.github.com>
2020-03-17 00:37:35 +0530

    db connect (#8)
    Co-authored-by: nagubash <ngurshareef123@gmail.com>

--- billing/src/app/add-items/service/add-items.service.ts
+++ billing/src/app/add-items/service/add-items.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BaseUrl } from 'src/app/BaseUrl';
 import { Items } from 'src/app/model/items';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,16 @@ import { Items } from 'src/app/model/items';
 export class AddItemsService {
   private item:Items[]=[];
   getData:string="/getData";
-
-  getItemData() {
-    this.http.get<Items[]>(this.url.base_url+this.getData).subscribe(res=>{
-      alert();
+  insertData:string="/insert";
+  
+  saveData(data: Items[]) {
+    this.http.post(this.url.base_url+this.insertData,data).subscribe(res=>{
+      console.log("insert");
     });
+  }
+
+  getItemData():Observable<Items[]> {
+    return this.http.get<Items[]>(this.url.base_url+this.getData);
   }
 
   constructor(private http:HttpClient,private url:BaseUrl) { }
